3c8ab8631da383a14c84f271b37f4ed2f62d4dd3
RuanPerroni <rrplongo@gmail.com>
2024-05-09 21:58:56 -0300

    feat: :art: add post component to app

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/RuanPerroni.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Ruan Perroní</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="09 de Maio às 08:13h" dateTime="2024-05-09 08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto #nlw #rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
+
+//aula: Formulário de comentários
