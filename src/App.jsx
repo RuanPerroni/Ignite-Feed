7ee955e770b924fb64aacc586395845bd93560f7
RuanPerroni <rrplongo@gmail.com>
2024-05-14 22:04:39 -0300

    feat: :art: add intering to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,48 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/RuanPerroni.png",
+      name: "Ruan Perroní",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+
+      { type: "link", content: "👉jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-03 20:00:00"),
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/MatheusBorgesDev.png",
+      name: "Matheus Borges",
+      role: "Web Developer",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+
+      { type: "link", content: "👉jane.design/doctorcare" },
+    ],
+    publishedAt: new Date("2022-05-10 20:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -14,8 +56,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Ruan Perroní" content="Fodaseeeeeeeeeeeee teste" />
-          <Post author="Matheus Borges" content="Fodaseeeeeeeeeeeee teste" />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
